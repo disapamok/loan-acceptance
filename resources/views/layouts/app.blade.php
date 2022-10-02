da7f70d57946f95a87dbad49f562c52ef566d1e4
Sahan Dissanayake | Disapamok <sahandizanayake@gmail.com>
2022-10-02 19:42:52 +0530

    Application tree

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,87 +23,98 @@
 
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    Welcome back {{ auth()->user()->name }}, Dashboard
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse"
-                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                    aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+        @if (auth()->check())
+            <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+                <div class="container">
+                    <a class="navbar-brand" href="{{ url('/') }}">
+                        Welcome back {{ auth()->user()->name }}, Dashboard
+                    </a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse"
+                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <!-- Left Side Of Navbar -->
+                        <ul class="navbar-nav mr-auto">
 
-                    </ul>
+                        </ul>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
+                        <!-- Right Side Of Navbar -->
+                        <ul class="navbar-nav ml-auto">
+                            <!-- Authentication Links -->
+                            @guest
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                                 </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                        onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
+                                @if (Route::has('register'))
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    </li>
+                                @endif
+                            @else
+                                <li class="nav-item dropdown">
+                                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                        {{ Auth::user()->name }}
                                     </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
+                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="{{ route('logout') }}"
+                                            onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                            {{ __('Logout') }}
+                                        </a>
 
-        <main class="py-4">
-            <div class="container dashboard-content">
-                <div class="row">
-                    <div class="col-lg-3 sidebar">
-                        <div class="greet">
-                            <h4>Hi, {{ auth()->user()->name }}</h4>
-                            <p>Please click the menu items to navigate</p>
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                            class="d-none">
+                                            @csrf
+                                        </form>
+                                    </div>
+                                </li>
+                            @endguest
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+
+            <main class="py-4">
+                <div class="container dashboard-content">
+                    <div class="row">
+                        <div class="col-lg-3 sidebar">
+                            <div class="greet">
+                                <h4>Hi, {{ auth()->user()->name }}</h4>
+                                <p>Please click the menu items to navigate</p>
+                            </div>
+                            <div class="menu">
+                                <ul>
+                                    <li>
+                                        <a href="{{ route('loan.index') }}"
+                                            class="{{ request()->url() == route('loan.index') ? 'active' : '' }}">Loans</a>
+                                    </li>
+                                    <li>
+                                        <a href="{{ route('customer.index') }}"
+                                            class="{{ request()->url() == route('customer.index') ? 'active' : '' }}">Customers</a>
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
-                        <div class="menu">
-                            <ul>
-                                <li>
-                                    <a href="{{ route('loan.index') }}"
-                                        class="{{ request()->url() == route('loan.index') ? 'active' : '' }}">Loans</a>
-                                </li>
-                                <li>
-                                    <a href="{{ route('customer.index') }}"
-                                        class="{{ request()->url() == route('customer.index') ? 'active' : '' }}">Customers</a>
-                                </li>
-                            </ul>
+                        <div class="col-lg-9 admin-container">
+                            @yield('content')
                         </div>
                     </div>
-                    <div class="col-lg-9 admin-container">
+                </div>
+            </main>
+        @else
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 mt-5">
                         @yield('content')
                     </div>
                 </div>
             </div>
-        </main>
+        @endif
     </div>
 </body>
 
